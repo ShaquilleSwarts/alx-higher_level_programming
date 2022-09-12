ae999f2c71b38c8220f1ed2bb7b4afe5552c1712
shaquilleswarts <shaquilleswarts12@gmail.com>
2022-09-12 17:35:25 +0200

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for x in (my_list):
-            print (*my_list)
-    except TypeError:
-        print("this is a type Error")
-    except: 
-        print("unknown error")
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
+    print()
+    return printed
 
