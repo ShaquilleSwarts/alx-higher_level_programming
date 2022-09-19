a53d7e3e0cdbc1aa2f8dafd493087381f603d5a9
shaquilleswarts <shaquilleswarts12@gmail.com>
2022-09-19 15:54:21 +0200

    doc string edit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+#0-rectangle.py
+"""
+define rectangle class
+"""
 class Rectangle:
     """
-    writes an empty calls named rectangle
+    writes an empty class named rectangle
     """
     pass
